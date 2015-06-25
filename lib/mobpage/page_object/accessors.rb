28b5beeb2706fe70db6ce1dab2422f87a1c64c6e
Milton Davalos <mdavalos@GNVmdavalosJUG5RP.local>
2015-06-25 12:07:27 -0400

    added methods to handle collections

--- lib/mobpage/page_object/accessors.rb
+++ lib/mobpage/page_object/accessors.rb
@@ -25,56 +25,109 @@ module PageObject
       end
     end
 
+    #TODO Refactor
     def button(name, identifier={}, &block)
       create_standard_methods(name, identifier)
+    end
+
+    def buttons(name, identifier={})
+      create_standard_methods_for_collection(name, identifier)
     end
 
     def switch(name, identifier={}, &block)
       create_standard_methods(name, identifier)
     end
 
+    def switches(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
+    end
+
     def pin(name, identifier={}, &block)
       create_standard_methods(name, identifier)
+    end
+
+    def pins(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
     end
 
     def link(name, identifier={}, &block)
       create_standard_methods(name, identifier)
     end
 
+    def links(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
+    end
+
     def text_field(name, identifier={}, &block)
       create_standard_methods(name, identifier)
+    end
+
+    def text_fields(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
     end
 
     def check_box(name, identifier={}, &block)
       create_standard_methods(name, identifier)
     end
 
+    def check_boxex(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
+    end
+
     def select_list(name, identifier={}, &block)
       create_standard_methods(name, identifier)
+    end
+
+    def select_lists(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
     end
 
     def item_list(name, identifier={}, &block)
       create_standard_methods(name, identifier)
     end
 
+    def item_lists(name, identifier={}, &block)
+      create_standard_methods_for_collections(name, identifier)
+    end
+
     def element(name, identifier={}, &block)
       create_standard_methods(name, identifier)
+    end
+
+    def elements(name, identifier={})
+      create_standard_methods_for_collection(name, identifier)
     end
 
     def radio_button(name, identifier={}, &block)
       create_standard_methods(name, identifier)
     end
 
+    def radio_buttons(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
+    end
+
     def label(name, identifier={}, &block)
       create_standard_methods(name, identifier)
+    end
+
+    def labels(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
     end
 
     def image(name, identifier={}, &block)
       create_standard_methods(name, identifier)
     end
 
+    def images(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
+    end
+
     def paragraph(name, identifier={}, &block)
       create_standard_methods(name, identifier)
+    end
+
+    def paragraphs(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
     end
 
     def dialog(name, identifier={}, &block)
@@ -85,13 +138,18 @@ module PageObject
       create_standard_methods(name, identifier)
     end
 
+    def spinners(name, identifier={}, &block)
+      create_standard_methods_for_collection(name, identifier)
+    end
+
     def view_switcher(name, identifier={}, &block)
       create_standard_methods(name, identifier)
     end
 
-    def elements(name, identifier={})
+    def view_switchers(name, identifier={}, &block)
       create_standard_methods_for_collection(name, identifier)
     end
+
   end
 end
 
